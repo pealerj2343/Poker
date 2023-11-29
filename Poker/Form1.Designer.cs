f0e3f398af5bb756882759d2c5c9de31a933c84b
jpealer2 <jpealer2@3310PC11>
2023-11-29 13:50:56 -0500

    Add project files.

---
+++ Poker/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace Poker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.deal_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(267, 317);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(466, 317);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // deal_btn
+            // 
+            this.deal_btn.Location = new System.Drawing.Point(603, 317);
+            this.deal_btn.Name = "deal_btn";
+            this.deal_btn.Size = new System.Drawing.Size(75, 23);
+            this.deal_btn.TabIndex = 2;
+            this.deal_btn.Text = "button1";
+            this.deal_btn.UseVisualStyleBackColor = true;
+            this.deal_btn.Click += new System.EventHandler(this.deal_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.deal_btn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button deal_btn;
+    }
+}
+
---
+++ Poker/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Poker
+{
+    public partial class Form1 : Form
+    {
+        List<Card> deck = new List<Card>();
+
+        List<Card> p1Hand = new List<Card>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            createDeck();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void createDeck()
+        {
+            for(int i = 0; i < 52; i++)
+            {
+                deck.Add(new Card(i % 12, i / 12));
+            }
+        }
+        private void deal()
+        {
+            Random rnd = new Random();
+            checkDeck();
+            int card = rnd.Next(0, deck.Count);
+            p1Hand.Add(deck[card]);
+            deck.RemoveAt(card);
+            deck.TrimExcess();
+
+            checkDeck();
+            card = rnd.Next(0, deck.Count);
+            p1Hand.Add(deck[card]);
+            deck.RemoveAt(card);
+            deck.TrimExcess();
+        }
+        private void checkDeck()
+        {
+            if(deck.Count == 0)
+            {
+                deal();
+            }
+            return;
+        }
+
+        private void displayHand()
+        {
+            label1.Text = p1Hand[0].Value + " " + p1Hand[0].Suit;
+            label2.Text = p1Hand[1].Value + " " + p1Hand[1].Suit;
+        }
+
+        private void deal_btn_Click(object sender, EventArgs e)
+        {
+            deal();
+            displayHand();
+        }
+    }
+}
+public class Card
+{
+    // ace is 0 king is 12
+    private int value;
+    public int Value
+    {
+        set
+        {
+            this.value = value;
+        }
+        get
+        {
+            return value;
+        }
+    }
+    //0 is spade 1 is hearts 2 is diamonds 3 is clubs
+    private int suit;
+    public int Suit
+    {
+        set
+        {
+            if(value < 4 && value >= 0)
+                this.suit = value;
+        }
+        get
+        {
+            return suit;
+        }
+    }
+    Bitmap image;
+    public Card(int val, int s)
+    {
+        value = val;
+        suit = s;
+
+    }
+}
